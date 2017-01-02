12e594bef8006a4ddf2c655f99aed08647805430
arno <arno@ag866.com>
2017-01-02 15:05:55 +0800

    20170102 modify scripts/domain.py static/config/domain.txt

--- navigation/models.py
+++ navigation/models.py
@@ -2,14 +2,15 @@
 from django.db import models
 
 class Domain(models.Model):
-	"""
-	用于存储域名，解析
-	"""
-	product = models.CharField(max_length=50, unique=True, verbose_name=u'所属产品')
-	abbr = models.CharField(max_length=20, verbose_name=u'简称')
-	domain = models.CharField(max_length=50, unique=True, verbose_name=u'域名')
-	record = models.CharField(max_length=20, verbose_name=u'解析记录')
+    """
+    用于存储域名，解析
+    """
+    product = models.CharField(max_length=50, unique=False, verbose_name=u'所属产品')
+    abbr = models.CharField(max_length=20, unique=True, verbose_name=u'简称')
+    domain = models.CharField(max_length=50, unique=False, verbose_name=u'域名')
+    record = models.CharField(max_length=20, verbose_name=u'解析记录')
 
-	def __unicode__(self):
-   		return u'{0} {1} {2} {3}'.format(self.product, self.abbr, self.domain, self.record)
-
+    def __unicode__(self):
+        return u'{0} {1} {2} {3}'.format(self.product, self.abbr, self.domain, self.record)
+    #**class Meta:**
+    #    **ordering = ['product', 'abbr']**
--- scripts/domain.py
+++ scripts/domain.py
@@ -3,14 +3,57 @@
 #Created By Arno on 20170101
 #Introduction: 用于同步域名列表
 
+import sys,os
+sys.path.append('/usr/local/sadweb')
+from sadweb import settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sadweb.settings")
 from navigation.models import Domain
-import sys
-sys.path.append('D:\django\sad')
 
-DomainTxt = '..\static\config\domain.txt'
-f = open(DomainTxt, 'r')
-print f.readlines()
-f.close
+RootPath = '/usr/local/sadweb'
+DomainTxt = RootPath + '/static/config/domain.txt'
+RecordDic = {}
+DomainList = []
+
+with open(DomainTxt, u'r') as f:
+    DomainListTmp = f.readlines()
+
+#print DomainListTmp
+
+def recorddic_db(RecordDic):
+    try:
+        D = Domain.objects.get(abbr=RecordDic['abbr'])
+        D.abbr = RecordDic['abbr']
+        D.product = RecordDic['product']
+        D.domain = RecordDic['domain']
+        D.record = RecordDic['record']
+        D.save()
+        #print " ".join(str(RecordDic) + "updated")
+    except:
+        D = Domain(product=RecordDic['product'], abbr=RecordDic['abbr'], domain=RecordDic['domain'], record=RecordDic['record'])
+        D.save()
+        #print u" ".join(str(RecordDic) + "inserted")
+
+for Record in DomainListTmp:
+    '''
+    获取域名列表
+    '''
+    if '#' not in Record and Record != '':
+        RecordDic['product'] = Record.replace('\n', '').split('|')[0]
+        RecordDic['abbr'] = Record.replace('\n', '').split('|')[1]
+        RecordDic['domain'] = Record.replace('\n', '').split('|')[2]
+        RecordDic['record'] = Record.replace('\n', '').split('|')[3]
+        recorddic_db(RecordDic)
+        #print RecordDic
+        #DomainList.append(RecordDic)
+        #print DomainList
+
+
+
+
+
+
+
+
 
 
 
